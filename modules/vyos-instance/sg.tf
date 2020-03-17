f8bf7a47757be5a5cce90a71aaa630383dae48bf
2d <lucascchanson@gmail.com>
2020-03-16 17:07:45 -0700

    fixing protocol

--- modules/vyos-instance/sg.tf
+++ modules/vyos-instance/sg.tf
@@ -6,7 +6,7 @@ resource "aws_security_group" "this" {
   ingress {
     from_port   = 22
     to_port     = 22
-    protocol    = "-1"
+    protocol    = "tcp"
     cidr_blocks = [var.ssh_ingress_cidr_block]
   }
 
